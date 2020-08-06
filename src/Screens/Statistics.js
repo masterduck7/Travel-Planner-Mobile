beacd24af9fc368872cc19c55488941e9b2f92c1
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2020-08-06 13:15:09 -0400

    Statistics view

--- src/Screens/Statistics.js
+++ src/Screens/Statistics.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import moment from 'moment';
 import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
-import { Card, ListItem, Divider } from 'react-native-elements';
+import { Card, Divider } from 'react-native-elements';
 import TouchableScale from 'react-native-touchable-scale';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -12,8 +12,19 @@ export default class Statistics extends Component{
     constructor(props){
         super(props)
         this.state={
-            trips: [],
-            userData: {}
+            totalFlights: 0,
+            totalFlightsCost: 0,
+            totalHotelNights: 0,
+            totalHotelNightsCost: 0,
+            totalActivities: 0,
+            totalActivitiesCost: 0,
+            totalCities: 0,
+            totalCityCosts: 0,
+            avgFlights: 0,
+            avgHotels: 0,
+            avgActivities: 0,
+            avgCityCosts: 0,
+            totalTrips: 0
         }
     }
 
@@ -21,14 +32,66 @@ export default class Statistics extends Component{
         this.setState({
             userData: this.props.route.params.userData
         })
-        //this.getData(this.props.route.params.userData)
+        this.getData(this.props.route.params.userData)
     }
 
-    componentWillUnmount(){
-        this.setState({
-            userData: {},
-            trips: []
-        })
+    getData(data){
+        axios.get(`https://travelplanner.lpsoftware.space/api/trips_user?userID=${data.userID}`,{
+            headers: {
+              'Authorization': `Bearer ${data.accessToken}`
+            }})
+            .then(res => {
+                if (!res.data["Error"]) {
+                    let TotalFlights = 0
+                    let TotalFlightsCost = 0
+                    let TotalHotelNights = 0
+                    let TotalHotelNightsCost = 0
+                    let TotalActivities = 0
+                    let TotalActivitiesCost = 0
+                    let TotalCities = 0
+                    let TotalCityCosts = 0
+                    res.data.forEach(trip => {
+                        trip.flights.forEach(flight => {
+                            TotalFlightsCost = Number(TotalFlightsCost) + Number(flight.price)
+                            TotalFlights = Number(TotalFlights) + Number(1)
+                        });
+                        trip.cities.forEach(city => {
+                            city.hotels.forEach(hotel => {
+                                let diff = moment(hotel.end_date).diff(moment(hotel.start_date), 'days')
+                                TotalHotelNights = Number(TotalHotelNights) + Number(diff)
+                                TotalHotelNightsCost = Number(TotalHotelNightsCost) + Number(hotel.total_price)
+                            });
+                            city.activities.forEach(activity => {
+                                TotalActivitiesCost = Number(TotalActivitiesCost) + Number(activity.total_price)
+                                TotalActivities = Number(TotalActivities) + Number(1)
+                            });
+                            TotalCities = Number(TotalCities) + Number(1)
+                            city.citycosts.forEach(citycost => {
+                                TotalCityCosts = Number(TotalCityCosts) + Number(citycost.total_price)
+                            });
+                        });
+                    });
+
+                    this.setState({
+                        totalFlights: TotalFlights,
+                        totalFlightsCost: TotalFlightsCost,
+                        totalHotelNights: TotalHotelNights,
+                        totalHotelNightsCost: TotalHotelNightsCost,
+                        totalActivities: TotalActivities,
+                        totalActivitiesCost: TotalActivitiesCost,
+                        totalCities: TotalCities,
+                        totalCityCosts: TotalCityCosts,
+                        avgFlights: Number(TotalFlightsCost/TotalFlights).toFixed(0),
+                        avgHotels: Number(TotalHotelNightsCost/TotalHotelNights).toFixed(0),
+                        avgActivities: Number(TotalActivitiesCost/TotalActivities).toFixed(0),
+                        avgCityCosts: Number(TotalCityCosts/TotalCities).toFixed(0),
+                        totalTrips: Number(TotalFlightsCost) + Number(TotalHotelNightsCost) + Number(TotalActivitiesCost) + Number(TotalCityCosts)
+                    })
+                }else{
+                    console.log("Error in Get All Trip data")
+                }
+            })
+            .catch(error => console.log(error))
     }
 
     goTo(e, view){
@@ -43,6 +106,88 @@ export default class Statistics extends Component{
                 <TouchableScale style={styles.buttonBack} friction={90} tension={100} onPress={(e) => this.goTo(e, 'Home')}>
                     <Ionicons style={styles.buttonBackIcon} name="md-arrow-round-back" size={20} color="white" />
                 </TouchableScale>
+                <View style={styles.stat}>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-stats' />  ${this.state.totalTrips}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            All trips costs
+                        </Text>
+                    </Card>
+                </View>
+                <View style={styles.stat}>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-briefcase' /> {this.state.totalFlights}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Flights
+                        </Text>
+                    </Card>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-card' /> {this.state.avgFlights}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Flights Average
+                        </Text>
+                    </Card>
+                </View>
+                <View style={styles.stat}>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-bed' /> {this.state.TotalHotelNights}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Hotel Nights
+                        </Text>
+                    </Card>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-card' /> {this.state.avgHotels}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Hotel Night Average
+                        </Text>
+                    </Card>
+                </View>
+                <View style={styles.stat}>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-basketball' /> {this.state.totalActivities}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Activities
+                        </Text>
+                    </Card>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-card' /> {this.state.avgActivities}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Activities Average
+                        </Text>
+                    </Card>
+                </View>
+                <View style={styles.stat}>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-pin' /> {this.state.totalCities}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            Cities
+                        </Text>
+                    </Card>
+                    <Card containerStyle={styles.statCard}>
+                        <Text style={styles.statText}>
+                            <Ionicons size={30} name='md-card' /> {this.state.avgCityCosts}
+                        </Text>
+                        <Text style={styles.statTextSub}>
+                            City cost Average
+                        </Text>
+                    </Card>
+                </View>
                 <Divider style={{marginTop: hp('2%')}} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>By LPSoftware</Text>
@@ -125,5 +270,24 @@ const styles = StyleSheet.create({
         color: 'black',
         fontWeight: 'bold',
         fontSize: hp('1.5%')
+    },
+    stat: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    statCard: {
+        width: wp('40%'),
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 2
+    },
+    statText: {
+        fontSize: hp('2.5%'),
+        textAlign: 'center'
+    },
+    statTextSub:{
+        textAlign: 'center',
+        fontSize: hp('1.5%'),
+        marginTop: hp('1%')
     },
 });
