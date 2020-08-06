7594fe519517028b0f8066b558bc78952a59e6d4
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2020-08-06 13:24:38 -0400

    Fix css

--- src/Screens/Statistics.js
+++ src/Screens/Statistics.js
@@ -202,25 +202,8 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column'
     },
-    containerList: {
-        marginLeft: '4%',
-        marginRight: '4%',
-        marginTop: '2%',
-        borderRadius: 20,
-        borderWidth: 0,
-        backgroundColor: '#2F496E'
-    },
-    titleStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: hp('2%')
-    },
-    subtitleStyle: {
-        color: 'white',
-        fontSize: hp('1.5%')
-    },
     titleMain: {
-        marginTop: hp('5%'),
+        marginTop: hp('7%'),
         marginBottom: hp('2%'),
         textAlign: 'center',
         fontWeight: 'bold',
@@ -233,26 +216,6 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         position: 'absolute',
         left: wp('4%'),
-        top: hp('5%'),
-        marginBottom: hp('-5%'),
-        padding: 8,
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    buttonText: {
-        fontSize: hp('2%'),
-        fontWeight: 'bold',
-        color: 'white',
-        textAlign: 'center'
-    },
-    buttonAdd: {
-        backgroundColor: '#2988BC',
-        width: wp('10%'),
-        height: hp('5%'),
-        borderRadius: 10,
-        position: 'absolute',
-        right: wp('4%'),
         top: hp('5%'),
         marginBottom: hp('-5%'),
         padding: 8,
